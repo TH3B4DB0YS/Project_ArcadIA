f2942ee5bcf2357d7b4bc2f4b212566eae42056f
TH3B4DB0YS <114828122+TH3B4DB0YS@users.noreply.github.com>
2026-04-17 14:52:09 +0200

    Add files via upload

---
+++ laboprogarcadeIA/Form1.cs
@@ -0,0 +1,33 @@
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace laboprogarcadeIA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            foreach (var user in )
+            {
+                
+
+            }
+
+            
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+        }
+    }
+}
